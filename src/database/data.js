85603fae1978faff0ce0846ca928afaad291b960
Dustin Siggelkow <unequivocable@gmail.com>
2021-08-30 15:50:11 -0600

    setup react site

---
+++ src/database/data.js
@@ -0,0 +1,28 @@
+
+
+const data = [
+    {
+    title: "Where it all started",
+    text: "... quote from the book about something.  (page 21).",
+    alt: "Wheatfields of Saskatchewan",
+    sm: "auto",
+    md: "auto",
+    lg: "auto",
+    color: "light",
+    bordercolor: "dark",
+    src: "/photos/pexels-pixabay-289334.jpg"
+  },
+  {
+      title: "My youngest grandson",
+      text: "... quote from the book about something.  (page 21).",
+      alt: "Bohdan James Siggelkow",
+      sm: "auto",
+      md: 6,
+      lg: 6,
+      color: "light",
+      bordercolor: "dark",
+      src: "/photos/982 (2).jpg"
+  }
+  ]
+
+  export default data;
