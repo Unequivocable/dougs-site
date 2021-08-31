e1fcc14cda6d7f16401536b7beda6d12a2edf3df
Dustin Siggelkow <unequivocable@gmail.com>
2021-08-31 10:52:10 -0600

    added firebase and photos

--- src/database/data.js
+++ src/database/data.js
@@ -8,21 +8,36 @@ const data = [
     sm: "auto",
     md: "auto",
     lg: "auto",
-    color: "light",
+    color: "primary",
     bordercolor: "dark",
-    src: "/photos/pexels-pixabay-289334.jpg"
+    src: "/photos/pexels-pixabay-289334.jpg",
+    rotate: "rotate-left"
   },
   {
       title: "My youngest grandson",
       text: "... quote from the book about something.  (page 21).",
       alt: "Bohdan James Siggelkow",
       sm: "auto",
-      md: 6,
-      lg: 6,
-      color: "light",
+      md: "auto",
+      lg: 5,
+      color: "secondary",
       bordercolor: "dark",
-      src: "/photos/982 (2).jpg"
-  }
+      src: "/photos/982.jpg",
+      rotate: "rotate-middle"
+  },
+  {
+    title: "Dark skies ahead",
+    text: "... another quote from the book about something.  (page 21).",
+    alt: "prarie skies",
+    sm: "auto",
+    md: "auto",
+    lg: 7,
+    color: "secondary",
+    bordercolor: "dark",
+    src: "/photos/pexels-gian-fabrizio-torre-3700908.jpg",
+    rotate: "rotate-right"
+}
+
   ]
 
   export default data;
--- src/sub-components/DesignPortfolio.js
+++ src/sub-components/DesignPortfolio.js
@@ -3,15 +3,15 @@ import { Card } from "react-bootstrap";
 
 const DesignPortfolio = (props) => {
   return (
-    <Card bg={props.color} border={props.bordercolor} style={{ width: "100%" }}>
+    <Card bg={props.color} border={props.bordercolor} className="new-border" style={{ width: "100%" }}>
       {/* {props.title ? (
         <Card.Header>
           <h2 className="center">{props.title}</h2>
         </Card.Header>
       ) : null} */}
-      <Card.Body>
-        <Card.Img variant="top" src={props.src} alt={props.alt} />
-        <Card.Title>{props.title}</Card.Title>
+      <Card.Body className="centre">
+        <Card.Img variant="top" src={props.src} alt={props.alt}  />
+        <Card.Title style={{ margin: "8px 0 0 0" }}><h3>{props.title}</h3></Card.Title>
         <Card.Text>
           <br />
           <p>{props.text}</p>
